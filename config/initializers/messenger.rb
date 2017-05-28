40780ca676e79531bfc09d03ef47af3134a04875
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2017-05-28 03:49:19 +0200

    Change verify token

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,4 +1,4 @@
 Messenger.configure do |config|
-  config.verify_token      = '<VERIFY_TOKEN>' #will be used in webhook verifiction
+  config.verify_token      = 'misiopysio123'
   config.page_access_token = 'EAAGgeIfHZAqUBABtMwvzBz2xKCTmfs3Uey4GK79uIkc3czCrjxZCmE8LnN3ZBMEYaWVhDsdpiBXkOEoJ9EvGYtKg5NQ3KZA1ZAoCwxLY2RkcpcuEa0eTaBSUhawnE8d9sx28q5JSsWLm6Pa9PLTuAffKVk9kGnZBruhUfF8pcwxQZDZD'
 end
